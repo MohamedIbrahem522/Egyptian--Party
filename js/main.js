1ffe2d627799a36f945670ad962c0e0ad0120c67
Mohamed Ibrahem <112443087+MohamedIbrahem522@users.noreply.github.com>
2022-10-03 01:20:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$(".nav-side a").click(function (e) {
+  let searchValue = e.target.getAttribute("href");
+
+  $("html,body").animate({ scrollTop: $(searchValue).offset().top }, 2000);
+});
+// ________________________________________________________
+$(".container h4").on("click", function () {
+  $(this).next(".container p").slideToggle();
+});
+
+//___________________(OPEN & CLOSE)______________________
+let divWidth = $(".nav-side").width();
+
+$(".open").click(function () {
+  if ($(".nav-side").css("left") == "0px") {
+    $(".nav-side").animate({ left: -divWidth });
+    $(`.open`).animate({ marginLeft: "0px" });
+  } else {
+    $(".nav-side").animate({ left: "0px" });
+    $(`.open`).animate({ marginLeft: "250px" });
+  }
+});
+
+$(".close").click(function () {
+  $(".nav-side").animate({ width: 0 });
+  $(`.open`).animate({ marginLeft: 0 });
+});
+// __________________________________________________________
+(function () {
+  let countDownDate = new Date("Dec, 2022 13:37:25").getTime();
+  let x = setInterval(function() {
+      let now = new Date().getTime();
+      let distance = countDownDate - now;
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+      cartona = `
+      <div class="col-md-3 s3">
+                      <div class="p-5 text-center border border-2 border-white text-white">
+                          <p class="fontchange">${days} D</p>
+                      </div>
+                  </div>
+                  <div class="col-md-3 s3">
+                      <div class="p-5 border text-center border-2 border-white text-white">
+                          <p class="fontchange">${hours} H</p>
+                      </div>
+                  </div>
+                  <div class="col-md-3 s3">
+                      <div class="p-5 border text-center border-2 border-white text-white">
+                          <p class="fontchange">${minutes} m</p>
+                      </div>
+                  </div>
+                  <div class="col-md-3 s3">
+                      <div class="p-5 border text-center border-2 border-white text-white">
+                          <p class="fontchange">${seconds} s</p>
+                      </div>
+                  </div>
+                  </div>
+      `
+      document.getElementById("due").innerHTML = cartona;
+      if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("due").innerHTML = "EXPIRED";
+        }
+      }, 1000);
+    
+})();
